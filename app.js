c4aa3d38d31e3ee22659d1d0947162bed6f9f76b
James Spicer <spicer.r.james@gmail.com>
2020-07-01 17:13:38 -0500

    added increment features to the scoreboard

--- app.js
+++ app.js
@@ -37,19 +37,49 @@ const Player = (props) => {
                 {props.name}
             </span>
 
-            <Counter score={props.score}/>
+            <Counter />
         </div>
     );
 }
 
-const Counter = (props) => {
-    return (
-    <div className="counter">
-        <button className="counter-action decrement"> - </button>
-    <span className="counter-score">{props.score}</span>
-        <button className="counter-action increment"> + </button>
-    </div>
-    )
+class Counter extends React.Component {
+
+    // constructor(){
+    //     super()
+    //     this.state = {
+    //         score: 0
+    //     };
+    // }
+
+    state = {
+        score: 0
+    };
+
+    incrementScore = () => {
+        this.setState( prevState => {
+          return  {
+                score: prevState.score + 1
+            }
+        });
+    }
+
+    decrementScore = () => {
+        this.setState(prevState => {
+            return {
+                score: prevState.score - 1
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="counter">
+                <button className="counter-action decrement" onClick={this.decrementScore.bind(this)}> - </button>
+                <span className="counter-score">{this.state.score}</span>
+                <button className="counter-action increment" onClick={this.incrementScore.bind(this)} > + </button>
+            </div>
+        );
+    }
 }
 
 const App = (props) => {
